5313464ed4dc64e06f82be251fa795be1c115f31
GSergeyP <93476215+GSergeyP@users.noreply.github.com>
2022-04-05 01:45:08 +0500

    Add files via upload

---
+++ js/sendDataServer.js
@@ -0,0 +1,31 @@
+//////////////////////////
+////      SESSION     ////
+//// Автор – GSergeyP ////
+////   Версия 1.0.0   ////
+//////////////////////////
+//Активация/Закрытие сессии
+function sendDataServer(element){
+    sessionAction = element.id; //Получение id по клику
+    //Отправка запроса sessionAction
+    fetch(url, {  
+        method: 'post',   
+        body: '{"sessionAction":"'+sessionAction+'","id_session":"'+id_session+'"}',
+        headers: new Headers()   
+    }).then(function (response){
+        return response.json();
+    }).then(function repeat(data){
+        id_session = data.id_session; //Получение переменной id_session
+
+        if(id_session){
+            localStorage.setItem('id_session', id_session); //Запись/Перезапись в localStorage
+            sessionOpen();
+        }
+        else{
+            localStorage.clear(); //Удаление в localStorage
+            sessionClose();
+        }
+    })  
+    .catch(function (error) {  
+        console.log('Запрос не выполнен', error);  
+    });
+} 
